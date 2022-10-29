9ef3fc8704c05af7e3ab845a1b459bcd7897f6da
Osnei Luzia <osnei.luzia@gmail.com>
2022-10-29 10:56:38 -0300

    Funcionalidade de Saldo

--- Projeto/script.js
+++ Projeto/script.js
@@ -59,6 +59,7 @@ function cadastrar(evento) {
 function disable(evento) {
     if (document.getElementById("operacaoSelect").value == "saldo") {
         document.getElementById("operacaoValor").disabled = true;
+        document.getElementById("operacaoValor").value = "";
     } else {
         document.getElementById("operacaoValor").disabled = false;
     }
@@ -73,35 +74,37 @@ function validacaoOperacao(evento) {
     const operacao = {
         Nome: document.getElementById(`usuario`).value,
         Senha: document.getElementById(`operacaoSenha`).value,
-        Operacao: document.getElementById(`operacaoSelect`).value
+        Operacao: document.getElementById(`operacaoSelect`).value,
+        Valor: document.getElementById(`operacaoValor`).value,
+        Controle: false
     }
-    let controle = false;
     for (let contClientes = 0; contClientes < clientes.length; contClientes++) {
         if (operacao.Nome == clientes[contClientes].Nome && operacao.Senha == clientes[contClientes].Senha) {
-            controle = true;
+            operacao.Controle = clientes[contClientes].ID;
         }
     }
-    if (!controle) {
+    if (!operacao.Controle) {
         alert(`Usuário e ou senha incorretos`)
     } else {
+
         switch (operacao.Operacao) {
-            case `saque`: saque()
+            case `saque`: saque(operacao)
                 break;
-            case `deposito`: deposito()
+            case `deposito`: deposito(operacao)
                 break;
-            case `saldo`: saldo()
+            case `saldo`: saldo(operacao)
                 break;
         }
     }
 }
-function saque() {
-    console.log("saque")
+function saque(operacao) {
+    alert("saque")
 }
-function deposito() {
-    console.log("deposito")
+function deposito(operacao) {
+    alert("deposito")
 }
-function saldo() {
-    console.log("saldo")
+function saldo(operacao) {
+    alert(`Saldo atual: ${clientes.find(operacao => operacao.Controle===clientes.ID).Saldo}`)
 }
 
 document.getElementById("operacao").addEventListener("submit", validacaoOperacao)
