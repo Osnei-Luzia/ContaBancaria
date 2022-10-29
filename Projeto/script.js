464845039b5a65029a90c0d0bbd9af0bfb9c3538
Osnei Luzia <osnei.luzia@gmail.com>
2022-10-29 10:38:53 -0300

    Validação de Operações

--- Projeto/script.js
+++ Projeto/script.js
@@ -8,40 +8,40 @@ let clientes = [{
     ID: 1000
 }];
 
-function validacao(cadastro) {
+function validacaoCadastro(cadastro) {
     let controle = ``;
-    if(cadastro.Senha===cadastro.ConfSenha){ 
+    if (cadastro.Senha === cadastro.ConfSenha) {
         let chaves = Object.keys(cadastro)
         for (let contClientes = 0; contClientes < clientes.length; contClientes++) {
             for (let cont = 0; cont <= 2; cont++) {// 2 numero de atributos do objeto
                 cadastro[chaves[cont]] == clientes[contClientes][chaves[cont]] ?
-                controle += `${chaves[cont]} Indisponível\n` : "";
+                    controle += `${chaves[cont]} Indisponível\n` : "";
             }
         }
-    }else{
-        controle = `Favo(00)02000-0002r confirmar Senha`
+    } else {
+        controle = `Favor confirmar Senha`
     }
 
-    if(controle){
+    if (controle) {
         alert(controle)
-    }else{
-        cadastro.ID = Math.floor(1000 + Math.random()*10000)//arrumar cadastro
+    } else {
+        cadastro.ID = Math.floor(1000 + Math.random() * 10000)//arrumar cadastro
         clientes.push(cadastro)
         let inputs = document.getElementsByTagName("input")
         alert(`Cadastro realizado com Sucesso\nSeu ID: ${cadastro.ID}`)
-        for(let cont=0;cont<inputs.length;cont++){
+        for (let cont = 0; cont < inputs.length; cont++) {
             inputs[cont].value = null
         }
     }
 }
 
-    //for(let contClientes=0;contClientes<=clientes.length;contClientes++){
-    //cadastro.key == clientes[cont].key?
-    //controle += `Nome indisponível `:"";
-    //cadastro.CPF == clientes[cont.CPF]?
-    //controle += `CPF indisponível `:"";
-    //cadastro.senha===cadastro.confSenha
-    //}
+//for(let contClientes=0;contClientes<=clientes.length;contClientes++){
+//cadastro.key == clientes[cont].key?
+//controle += `Nome indisponível `:"";
+//cadastro.CPF == clientes[cont.CPF]?
+//controle += `CPF indisponível `:"";
+//cadastro.senha===cadastro.confSenha
+//}
 
 function cadastrar(evento) {
     evento.preventDefault()
@@ -49,20 +49,59 @@ function cadastrar(evento) {
         Nome: document.getElementById(`nome`).value,
         CPF: document.getElementById(`cpf`).value,
         Celular: document.getElementById(`celular`).value,
-        Senha: document.getElementById(`senha`).value,
+        Senha: document.getElementById(`cadastroSenha`).value,
         ConfSenha: document.getElementById(`confSenha`).value,
         ID: 0,
         Saldo: 0,
     }
-    validacao(cadastro)
+    validacaoCadastro(cadastro)
 }
-function disable(evento){
-    if(document.getElementById("operacaoSelect").value=="saldo"){
+function disable(evento) {
+    if (document.getElementById("operacaoSelect").value == "saldo") {
         document.getElementById("operacaoValor").disabled = true;
-    }else{
+    } else {
         document.getElementById("operacaoValor").disabled = false;
     }
 }
 
 document.getElementById("cadastro").addEventListener("submit", cadastrar)
 document.getElementById("operacao").addEventListener("change", disable)
+window.addEventListener("load", disable)
+
+function validacaoOperacao(evento) {
+    evento.preventDefault()
+    const operacao = {
+        Nome: document.getElementById(`usuario`).value,
+        Senha: document.getElementById(`operacaoSenha`).value,
+        Operacao: document.getElementById(`operacaoSelect`).value
+    }
+    let controle = false;
+    for (let contClientes = 0; contClientes < clientes.length; contClientes++) {
+        if (operacao.Nome == clientes[contClientes].Nome && operacao.Senha == clientes[contClientes].Senha) {
+            controle = true;
+        }
+    }
+    if (!controle) {
+        alert(`Usuário e ou senha incorretos`)
+    } else {
+        switch (operacao.Operacao) {
+            case `saque`: saque()
+                break;
+            case `deposito`: deposito()
+                break;
+            case `saldo`: saldo()
+                break;
+        }
+    }
+}
+function saque() {
+    console.log("saque")
+}
+function deposito() {
+    console.log("deposito")
+}
+function saldo() {
+    console.log("saldo")
+}
+
+document.getElementById("operacao").addEventListener("submit", validacaoOperacao)
